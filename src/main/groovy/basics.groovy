e66975446bc594fc6ca2ea73ab05f681f9d24f3c
mathieucarbou <mathieu.carbou@gmail.com>
2011-12-01 16:14:07 -0500

    examples

--- src/main/groovy/basics.groovy
+++ src/main/groovy/basics.groovy
@@ -5,5 +5,33 @@ import redis.clients.jedis.Transaction
 @Grab('commons-pool:commons-pool:1.5.6')
 
 Jedis jedis = new Jedis('localhost')
+
 jedis.flushAll()
 
+println '--- increments ---'
+println jedis.incr('counter')
+println jedis.incr('counter')
+
+println '--- expiration ---'
+jedis.set "session-1", "data"
+jedis.expire "session-1", 2
+println jedis.get("session-1")
+sleep(3000)
+println jedis.get("session-1")
+
+println '--- maps ---'
+jedis.hset "members", "1", "mathieu"
+jedis.hset "members", "2", "david"
+println jedis.hgetAll("members")
+
+println '--- strings ---'
+jedis.set "welcome", "hello"
+println jedis.get("welcome")
+jedis.append 'welcome', ' world !'
+println jedis.get("welcome")
+
+println '--- lists ---'
+jedis.lpush "list", "2"
+jedis.lpush "list", "1"
+jedis.rpush "list", "3"
+println jedis.lrange("list", 0, Long.MAX_VALUE)
